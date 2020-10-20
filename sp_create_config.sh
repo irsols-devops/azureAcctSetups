b98b3bebc1d37e59e4fdb2416cc8a150ecab5f4d
irsols-devops <devops@irsols.com>
2020-10-20 17:33:53 -0500

    0.1.5

--- sp_create_config.sh
+++ sp_create_config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Provided by IRSOLS DevOps team 
-# IRSOLS Inc @irsols 
+# Provided by IRSOLS DevOps team
+# IRSOLS Inc @irsols
 # www.irsols.com / Your Cloud Native Edge
-# MIT License. Attribution required 
-# References: 
+# MIT License. Attribution required
+# References:
 # Azure CLI: https://docs.microsoft.com/en-us/cli/azure/ad?view=azure-cli-latest
 # AZ Service Principal : https://docs.microsoft.com/en-us/azure/active-directory/develop/app-objects-and-service-principals
 if [ -z "$1" ]
@@ -25,6 +25,7 @@ echo " This tool will setup a Service Principal with Client Secret for TF"
 # Once logged in - get list of all Subscriptions associated with the account using:
 # az account list
 export current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+export appcredsfile="AppCreds$current_time.txt"
 export defaultSubId=`az account list | grep -4 isDefault | grep -2 true | grep id | awk '{ print $2 }'| sed 's/\"//g
 '| sed 's/,//g'`
 
@@ -32,4 +33,26 @@ echo "Default Subscription ID is: $defaultSubId"
 echo "Creating the Service Principal and associated App Id, Passwd and Tokens"
 echo " Will be stored in the same directory with suffix AppCredsDDTT.txt"
 az account set --subscription="$defaultSubId"
-az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$defaultSubId" -n $1 > AppCreds$current_time.txt
+az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$defaultSubId" -n $1 > $appcredsfile
+
+# At this point the application exists in Azure Active Directory
+# we can now use it to modify  resources in the Subscription by setting following env variables
+# These will be ephemeral in the command window which is preferred
+# ** not recommended to set in the TF files.
+
+#export ARM_SUBSCRIPTION_ID="$defaultSubId"
+#export ARM_CLIENT_ID="<appId>"
+#export ARM_CLIENT_SECRET="<password>"
+#export ARM_TENANT_ID="<tenant>"
+export clientID=`grep appId $appcredsfile        | awk '{print $2}'| sed s/\"//g | sed s/,//g`
+export clientSecret=`grep password $appcredsfile | awk '{print $2}'| sed s/\"//g | sed s/,//g`
+export tenantId=`grep tenant $appcredsfile       | awk '{print $2}'| sed s/\"//g | sed s/,//g`
+
+
+echo "export ARM_SUBSCRIPTION_ID=\"$defaultSubId\"" > set_azvars_for_tf.txt
+echo "export ARM_CLIENT_ID=$clientID" >> set_azvars_for_tf.txt
+echo "export ARM_CLIENT_SECRET=$clientSecret" >> set_azvars_for_tf.txt
+echo "export ARM_TENANT_ID=$tenantId" >> set_azvars_for_tf.txt
+
+echo "Final manual step is to execute:"
+echo "source ./set_azvars_for_tf.txt" 
